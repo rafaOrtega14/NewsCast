23d19e9a876e2ba7d667c3ad34e4cad65ffdd1d3
Mac <mac@MacBook-Pro-de-Mac.local>
2017-09-20 00:22:17 +0200

    make it better

---
+++ routes/database/schema.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var userSchema = new Schema({
+  name: String,
+  id:  Number
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var schema = mongoose.model('schema', userSchema);
+
+// make this available to our users in our Node applications
+module.exports = schema;
