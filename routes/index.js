8c1a51f17effed106c3acbebfbcd4f017548d382
Mac <mac@MacBook-Pro-de-Mac.local>
2017-09-20 01:39:26 +0200

    make it

--- routes/index.js
+++ routes/index.js
@@ -10,24 +10,24 @@ router.get('/watch', function(req, res, next) {
   res.render('watch');
 });
 router.get('/getstreamid',function(req,res,next){
-  User.findOne({ name: 'gallo' }, function(err, user) {
+  User.findOne({ name: 'pepe' }, function(err, user) {
   if (err) throw err;
   res.send(user.id);
 });
 });
 router.post('/InsertStreamid',function(req,res,next){
 var user = new User({
-  name: 'gallo',
+  name: 'pepe',
   id: req.body.id
 });
-User.findOne({ name: 'gallo' }, function(err, user) {
+User.findOne({ name: 'pepe' }, function(err, user) {
 if (user.length==0){
   user.save(function(err) {
     if (err) throw err;
     res.send(user.length);
   });
 }else{
-  User.findOneAndUpdate({ name: 'gallo' }, { id: req.body.id }, function(err, user) {
+  User.findOneAndUpdate({ name: 'pepe' }, { id: req.body.id }, function(err, user) {
   if (err) throw err;
   res.send("update...")
 });
