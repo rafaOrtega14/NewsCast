dcf0905f6561fbd05e710cac27dbc868f44b03dd
Mac <mac@MacBook-Pro-de-Mac.local>
2017-09-29 16:47:58 +0200

    daadsf

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.get('/sendmsg',function(req,res,next){
    },
    url: 'https://AC375edcf5add139df1fb3c4b3d48943d6:e56eaee02ce230889aa65f7e18e443a1@tadhack.restcomm.com/restcomm/2012-04-24/Accounts/AC375edcf5add139df1fb3c4b3d48943d6/SMS/Messages',
    method: 'POST',
-   json: json
+   body: json
  };
 
  request(options, function(err, rese, body) {
