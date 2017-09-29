06ae195ad524a1da3e6b866fddb3bfb56c84bdb2
Mac <mac@MacBook-Pro-de-Mac.local>
2017-09-29 17:25:12 +0200

    asdfadsf

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  req.session.user=req.body.user;
   User.find({ live:true }, function(err, streams) { //mongo call User(database schema) find all streams that are live
     if (err){ //something went wrong
       console.log("error");
@@ -51,7 +52,7 @@ router.get('/register', function(req, res, next){
 
 /* GET id from the user that is streaming */
 router.get('/getstreamid',function(req,res,next){
-  User.findOne({ name: 'popo' }, function(err, user) { //mongo call that get user information of an user
+  User.findOne({ name: 'pedro' }, function(err, user) { //mongo call that get user information of an user
   if (err) res.send("0"); //if something went wrong 0
   else {
     if(user=="") res.send("0"); //if user doesnt exists 0
@@ -61,16 +62,16 @@ router.get('/getstreamid',function(req,res,next){
 });
 /* GET that update stream to shut it down*/
 router.get('/EndStream',function(req,res,next){
-  User.findOneAndUpdate({ name: 'popo' }, { live: false}, function(err, user) { //mongo call find user and UPDATE him
+  User.findOneAndUpdate({ name: req.session.user }, { live: false}, function(err, user) { //mongo call find user and UPDATE him
   if (err) throw err;
   res.redirect('/'); //we redirect to our index main page just to close application flow
 });
 });
 /* POST used to insert the stream id */
 router.post('/InsertStreamid',function(req,res,next){
-User.findOne({ name: 'popo' }, function(err, usere) { //mongo get information from the user
+User.findOne({ name: req.session.user  }, function(err, usere) { //mongo get information from the user
   var user = new User({
-    name: 'popo', //user name
+    name: req.session.user , //user name
     id: req.body.id, //stream id
     room: req.body.room, //room where the stream is    /* JSON object use to insert in mongo*/
     live: true //naturally the user is streaming so TRUE
@@ -81,7 +82,7 @@ if (usere==null){ //if user doesnt exists
     res.send(req.body.room); //response just to test
   });
 }else{
-  User.findOneAndUpdate({ name: 'popo' }, /* Find user and update the new room new stream id and put live true*/
+  User.findOneAndUpdate({ name: req.session.user  }, /* Find user and update the new room new stream id and put live true*/
   { id: req.body.id,room:req.body.room,live:true }, function(err, user) {
   if (err) throw err;
   res.send("update...") //response just to test
